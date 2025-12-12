c68ed72e6e34e52230084abe0cb4f2f43da9c4e5
Paul Hamshere <p.w.hamshere@gmail.com>
2025-12-12 11:03:43 +0000

    Add files via upload

--- tests/test7.rs
+++ tests/test7.rs
@@ -3,25 +3,25 @@ start:
 another_label:
 another:
 // with comment
-    ld  x3, x2(0)            
-    ld  x1, x2[1]   
+    lw   x3, x2(0)            
+    lw   x1, x2[1]   
 jump_point:
 here:
-    add x2, x3, x1           
-    jmp -8                 
-    st  x2, x1 + 0            
-    sub x2, x3, x1           
-    inv x2  x3               
-    lsl x2  x3  x1           
-    lsr x2, x3, x1           
-    and x2, x3, x1           
-    or  x2, x3, x1           
-    slt x2, x3, x1           
-    add x3, x3, x3           
-    beq x3, x2, 4            // {jump 60}
-    bne x3, x2, 0            // {jump 60}
-    jmp another              
+    add  x2, x3, x1           
+    jal  x0, -8                 
+    sw   x2, x1 + 0            
+    xor  x2, x3, x1           
+    addi x2  x3  12               
+    sll  x2  x3  x1           
+    slt  x2, x3, x1           
+    and  x2, x3, x1           
+    or   x2, x3, x1           
+    sltu x2, x3, x1           
+    add  x3, x3, x3           
+    beq  x3, x2, 4            // {jump 60}
+    bne  x3, x2, 0            // {jump 60}
+    jal  x0, another              
 cont:
-    ld  x1, x2(5)            // comment
-    st  x3, x5(3)            // this is
-     add x4, x2, x1           // a
+    lw   x1, x2(5)            // comment
+    sw   x3, x5(3)            // this is
+    add  x4, x2, x1           // a
--- tests/test8.rs
+++ tests/test8.rs
@@ -1,17 +1,17 @@
 interim:
 start:
-    sub x7, x3, x3           // comment
-    lsl x6, x4, x1           
-    lsr x5, x5, x2           
-    slt x4, x6, x3           
-    and x3, x7, x4           
-    or  x2, x5, x6           
-    inv x1, x2               
-    beq x1, x2, 4
-    beq x1, x2, -8      
-    bne x1, x2, end   
-    lui x5, x5, 103
-    jal x0,interim              
+    sub  x7, x3, x3           // comment
+    sll  x6, x4, x1           
+    andi x5, x5, 10         
+    slt  x4, x6, x3           
+    and  x3, x7, x4           
+    or   x2, x5, x6           
+    add  x1, x2, x3               
+    beq  x1, x2, 4
+    beq  x1, x2, -8      
+    bne  x1, x2, end   
+    lui  x5, x5, 103
+    jal  x0,interim              
 end:
-    jal x0, end                  
-    jal x0 start
+    jal  x0, end                  
+    jal  x0 start
